e99b707b04ceafc5d1db6bbcb6cada8931a955d3
thePeeyush <peeyushdehariya951@gmail.com>
2024-02-28 19:23:29 +0530

    feat:start connection at first load

--- app/page.tsx
+++ app/page.tsx
@@ -7,23 +7,33 @@ import { useConnection } from "@/store/connection";
 import { usePeer } from "@/store/peer";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 import { BsDownload, BsUpload } from "react-icons/bs";
 
 export default function Home() {
 
   const setMyID = usePeer(s => s.setMyID)
+  const myID = usePeer(s => s.myID)
   const setReciever = useConnection(s => s.setReciever)
   const setSender = useConnection(s => s.setSender)
   const setStartSession = useConnection(s => s.setStartSession)
 
-  setSender(false)
-  setReciever(false)
+  
+
+  useEffect(()=>{
+    setSender(false)
+    setReciever(false)
+    startSession()
+  },[])
 
 
-  const startSession = async (key: string) => {
+  const startSession = async () => {
     setStartSession(true)
     const id = await PeerConnection.startPeerSession()
     setMyID(id)
+  }
+
+  const chooseMode = async(key:string) =>{
     key === 'sender' && setSender(true)
     key === 'reciever' && setReciever(true)
   }
@@ -49,11 +59,11 @@ export default function Home() {
         <p className="text-center text-slate-500">Share any file on any device securely and seamlessly</p>
       </div>
       <div className="mt-6 flex">
-        <Link onClick={() => startSession('sender')} href={'/peer'}><Btn label="send"><BsUpload className=" text-5xl animate-bounce text-orange-600 group-hover:text-7xl" /></Btn></Link>
-        <Link onClick={() => startSession('reciever')} href={'/peer'}><Btn label="recieve"><BsDownload className="text-5xl group-hover:text-7xl  animate-bounce text-green-600" /></Btn></Link>
+        <Link onClick={() => chooseMode('sender')} href={'/peer'}><Btn label="send"><BsUpload className=" text-5xl animate-bounce text-orange-600 group-hover:text-7xl" /></Btn></Link>
+        <Link onClick={() => chooseMode('reciever')} href={'/peer'}><Btn label="recieve"><BsDownload className="text-5xl group-hover:text-7xl  animate-bounce text-green-600" /></Btn></Link>
       </div>
       <div>
-        <LinkShare onClick={() => startSession('link')} />
+        <LinkShare/>
       </div>
     </main>
   );
--- components/link.tsx
+++ components/link.tsx
@@ -8,11 +8,12 @@ import { PeerConnection } from '@/lib/peer'
 import { useConnection } from '@/store/connection'
 import { useRouter } from 'next/navigation'
 
-const LinkShare = ({ onClick }: { onClick: React.MouseEventHandler }) => {
+const LinkShare = () => {
     const [clicked, setClicked] = useState(false)
     const [generated, setGenerated] = useState(false)
-    const setPeerID = usePeer(s=>s.setPeerID)
-    const setIsConnected =  useConnection(s=>s.setIsConnected)
+    const [copied, setCopied] = useState(false)
+    const setPeerID = usePeer(s => s.setPeerID)
+    const setIsConnected = useConnection(s => s.setIsConnected)
     const router = useRouter()
 
     const myID = usePeer(s => s.myID)
@@ -20,46 +21,38 @@ const LinkShare = ({ onClick }: { onClick: React.MouseEventHandler }) => {
         if (myID !== '') setGenerated(true)
         try {
             PeerConnection.onIncomingConnection((conn) => {
-              const id = conn.peer
-              setPeerID(id)
-              setIsConnected(true)
-              router.push('/peer')
+                const id = conn.peer
+                setPeerID(id)
+                setIsConnected(true)
+                router.push('/peer')
             })
-          } catch (error) {
+        } catch (error) {
             console.log(error);
-        
-          }
+
+        }
     }, [myID])
 
-    return (
+    useEffect(()=>{
+        if (!copied) {
+           copyLink() 
+        }
+    },[generated,clicked])
 
-        generated ? <GeneratedLink myID={myID} /> : (
-            clicked ? <Button variant={'outline'} className='rounded-full'>Generating...</Button> : (
-                <Button variant={'outline'} onClick={onClick} className='rounded-full'><h1 onClick={() => setClicked(true)} >Share Link</h1></Button>
-            )
-        )
+    const copyLink = async () => {
+        if (generated && clicked) {
+            const link = `${getBaseURL()}peer?peerID=${myID}`
+                await navigator.clipboard.writeText(link)
+                setCopied(true)
+        }
+    }
+
+    return (
+        <Button onClick={()=>setClicked(true)} variant='outline' className='rounded-full'>{
+            copied ? <>Copied</>
+                : (clicked && !generated ? <>Generating...</>
+                    : <>Share Link</>)
+        }</Button>
     )
 }
 
 export default LinkShare
-
-const GeneratedLink = ({ myID }: { myID: string }) => {
-
-    const link = `${getBaseURL()}peer?peerID=${myID}`
-    const [copied, setCopied] = useState(false)
-
-    const handleCopyClick = async () => {
-        await navigator.clipboard.writeText(link)
-        setCopied(true)
-    };
-
-
-    return (
-        <div className='flex flex-col justify-center items-center gap-2'>
-            <p className='text-xs bg-gray-100 px-5 p-1 rounded-full'>{link}</p>
-            {
-                !copied ? <Button className='rounded-full' onClick={handleCopyClick}>Copy Link</Button> : <Button variant={'outline'} className='rounded-full'>Copied</Button>
-            }
-        </div>
-    )
-}
